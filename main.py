5b4425a700f11795c21ee52b68fb7ab6d84a34c0
maru-stack <kou9518@gmail.com>
2025-06-29 15:01:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-app = Flask(__name__)
-
-import os
-from flask import Flask, request
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError 
-
+# Render の Environment に登録した値を読み込む
 line_bot_api = LineBotApi(os.getenv("CHANNEL_ACCESS_TOKEN"))
 handler      = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 
-@app.route("/callback", methods=['POST'])
+app = Flask(__name__)
+
+@app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    handler.handle(body, signature)
-    return 'OK'
+    signature = request.headers["X-Line-Signature"]
+    body      = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)        # 署名が違うときは 400 を返す
+    return "OK"
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    reply = f'あなたは「{msg}」と書きましたね！'
+    msg = event.message.text.strip()
+
+    # === ここが応答ロジック ===
+    if msg == "北斗500":
+        reply = "打てる"
+    else:
+        reply = "ごめん、分からない！"
+    # ========================
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply)
